e850b9968c7bcb357524025db45d235551ca0b1c
rrecker01 <ryanrecker1@gmail.com>
2023-10-22 14:15:16 -0400

    ctc_track_controller_api updated, includes variables, setters, and getters

--- api/ctc_track_controller_api.py
+++ api/ctc_track_controller_api.py
@@ -1,4 +1,41 @@
 class CTCTrackControllerAPI:
     def __init__(self):
         # Define variable passed between CTC and Track Controller\
-        self.dummy = ""
+
+        #CTC to Track Controller
+        self._authority = 0.0 # meters
+        self._track_section_status = False # is the track in maintanence mode
+        self._suggested_speed = 0.0 # meters/sec
+
+        #Track Controller to CTC
+        self._passenger_onboarding = 0 # tickets sold
+        self._occupancy = {'block': {1: 0.0, 2: False}} # blocks{speed limit(m/s), occupancy(bool)}
+
+    def get_authority(self) -> float:
+        return self._authority
+    def get_track_section_status(self) -> bool:
+        return self._track_section_status
+
+    def get_suggested_speed(self) -> float:
+        return self._suggested_speed
+
+    def get_passenger_onboarding(self) -> int:
+        return self._passenger_onboarding
+
+    def get_occupancy(self, block) -> bool:
+        return self._occupancy[block][2]
+
+    def set_authority(self, distance: float):
+        self._authority = distance
+
+    def set_track_section_status(self, mode: bool):
+        self._track_section_status = mode
+
+    def set_suggested_speed(self, speed: float):
+        self._suggested_speed = speed
+
+    def set_passenger_onboarding(self, tickets: int):
+        self._passenger_onboarding = tickets
+
+    def set_occupancy(self, block, value: bool):
+        self._occupancy[block][2] = value
