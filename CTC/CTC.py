d39f81459156e423ca3f92d81012e9658484c447
itsmekhalidb <130712144+itsmekhalidb@users.noreply.github.com>
2023-10-25 19:21:42 -0400

    Added CTC to launcher

--- CTC/CTC.py
+++ CTC/CTC.py
@@ -1,17 +1,22 @@
 # -- Imports -- #
+import traceback
 import os
 import threading
 from datetime import datetime, timedelta
+
+from api.ctc_track_controller_api import CTCTrackControllerAPI
 
 
 _stations = {} # dictionary of stations and block ids
 
 
 class CTC(object):
-    def __init__(self):
+    def __init__(self, TrackCTRLSignal : CTCTrackControllerAPI):
         # -- CTC Variables -- #
         self._track = None # object for the track
         self._trains = [] # list of train objects
+
+        self.TrackCTRLSignal = TrackCTRLSignal
 
         # run update function
         self.update()
@@ -60,6 +65,16 @@ class CTC(object):
         # Enable Threading
         if thread:
             threading.Timer(0.1, self.update).start()
+
+    def launch_ui(self):
+        print("Launching CTC UI")
+        try:
+            from CTC.CTC_UI import CTC_Main_UI
+            self._ui = CTC_Main_UI(self)
+        except Exception as ex:
+            print("An error occurred:")
+            traceback.print_exc()
+            print("CTC not initialized")
 
 
 
