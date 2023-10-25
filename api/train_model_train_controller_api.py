c532dcaac80749cdcace17c25dd2049e7c0dd000
ldt9 <84938803+ldt9@users.noreply.github.com>
2023-10-24 23:42:46 -0400

    Updated TrackModelTrainModelAPI vars/library, implemented block and passenger update functions for train model

--- api/train_model_train_controller_api.py
+++ api/train_model_train_controller_api.py
@@ -1,32 +1,34 @@
 class TrainModelTrainControllerAPI:
     def __init__(self) -> None:
-        # Define variable passed between Train Model and Train Controller
+        ''' Define variables passed between Train Model and Train Controller '''
 
         # From Train Model to Train Controller
-        self.line = "" # track model
-        self.beacon = "" # track model
-        self.authority = 0 # track model
-        self.cmd_speed = 0 # track model
-        self.speed_limit = 0 # track model
-        self.underground = False # track model
-        self.station_side = "" # track model
+        ## Track Model Information
+        self.line = ""
+        self.beacon = ""
+        self.authority = 0
+        self.cmd_speed = 0
+        self.speed_limit = 0
+        self.underground = False
+        self.station_side = ""
+        self.time = 0
 
+        ## Failure Information
         self.signal_pickup_failure = False
         self.engine_failure = False
         self.brake_failure = False
         self.ebrake_failure = False
 
-        self.time = 0
+        ## Train Information
         self.actual_velocity = 0
+        self.temperature = 0
 
         # From Train Controller to Train Model
         self.cmd_power = 0.0
         self.emergency_brake = False
-        # self.service_brake = False
         self.service_brake_value = 0.0
         self.left_doors = False
         self.right_doors = False
         self.int_lights = False
         self.ext_lights = False
         self.temp_sp = 0.0
-        self.temperature = 0
