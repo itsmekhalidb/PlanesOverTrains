1eefe032cea64bb56c3ec2c586cda1419e889d80
Travis Al-Labon <98852320+tallabon8@users.noreply.github.com>
2023-11-15 23:36:24 -0500

    Train Controller working

--- train_controller/Train_Controller_Main_Window.py
+++ train_controller/Train_Controller_Main_Window.py
@@ -952,7 +952,7 @@ class Ui_MainWindow(QMainWindow):
         self.signal_fail_on.setVisible(bool(self.train_controller.get_signal_pickup_failure()))
         self.signal_fail_off.setVisible(not bool(self.train_controller.get_signal_pickup_failure()))
         #speed limit
-        self.external_lights_label_9.setText(str("Speed Limit (mph): " + str(self.train_controller.get_maximum_velocity())))
+        self.external_lights_label_9.setText(str("Speed Limit (mph): " + str(round(self.train_controller.get_maximum_velocity()*2.23694,3))))
         #commanded speed
         if self.train_controller.get_auto_status():
                 self.external_lights_label_10.setText(str("Com. Speed (mph): " + str(round(self.train_controller.get_commanded_velocity()*2.23694,3))))
