a01f52238237b89076e333d5354656cb63940e86
rrecker01 <ryanrecker1@gmail.com>
2023-11-26 14:15:07 -0500

    can change between wayside controllers on the green line and will have updated ui based on selected controller

--- track_controller/track_controller.py
+++ track_controller/track_controller.py
@@ -12,9 +12,9 @@ class Track_Controller(object):
         # blocks that are currently occupied
         self._occupied_blocks = []
         # 1 = red, 0 = green
-        self._lights = {'1': 0, '13': 0, '29': 0, '150': 0}
+        self._lights = {'1': 0, '13': 0, '28': 0, '150': 0, "62" : 0, "76" : 0, "77" : 0, "85" : 0, "100" : 0}
         # 1 = left, 0 = right
-        self._switches = {'13': 0, '29': 0, '57': 0, '63': 0, '76': 0, '85': 0}
+        self._switches = {'13': 0, '28': 0, '57': 0, '63': 0, '76': 0, '85': 0}
         # crossing lights/gate
         self._crossing_lights_gates = {'18': 0}
         # if program is in automatic mode
