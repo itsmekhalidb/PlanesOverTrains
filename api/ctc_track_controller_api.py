fbb3dd09f5315ff62529eaf1ae098d2fe3fea0f5
rrecker01 <ryanrecker1@gmail.com>
2023-12-08 14:11:17 -0500

    API changes and updated railway crossing in UI

--- api/ctc_track_controller_api.py
+++ api/ctc_track_controller_api.py
@@ -20,8 +20,14 @@ class CTCTrackControllerAPI:
         self._curr_speed = {}
         self._passenger_onboarding = 0 # tickets sold
         self._occupancy = {} # trains and their occupied blocks
-        self._light = {'1': 0, '13': 0, '29': 0, '150': 0, '77': 0, '100': 0, '85': 0, '63': 0, '76': 0, '101': 0} # light color, color (0 for green, 1 for red)
-        self._switch = {'13': 0, '28': 0, '57': 0, '63': 0, '77': 0, '85': 0} # switch position (first is block, second is position)
+        # 1 = red, 0 = green
+        self._lights = {"Green": {'1': 0, '13': 0, '28': 0, '150': 0, "62": 0, "76": 0, "77": 0, "85": 0,
+                                  "100": 0},
+                        "Red": {'1': 0, '15': 0, '16': 0, '10': 0, '76': 0, '72': 0, '71': 0,
+                                '67': 0, '52': 0, '53': 0, '66': 0}}
+        # 1 = left, 0 = right
+        self._switches = {"Green": {'13': 0, '28': 0, '57': 0, '63': 0, '77': 0, '85': 0},
+                          "Red": {'16': 0, '9': 0, '27': 0, '33': 0, '38': 0, '44': 0, '52': 0}}
         self._green_cutoffs = {"Green 1" : ['A1::I52', 'W127::Z150'], "Green 2" : ['I53::W126']}
         self._filepath = ""
         self._track_info = {}
